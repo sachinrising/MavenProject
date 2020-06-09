b4f0b54e4bf64832251e2ad45124949fe762b473
sachin <sachinrising@gmail.com>
2020-06-10 00:05:53 +0530

    first commit

---
+++ src/main/java/com/sac/app/App.java
@@ -0,0 +1,15 @@
+package com.sac.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		// Change the string here...
+		String name = "Anything!!!...Which is not funny...";
+        System.out.println( name );
+    }
+}
